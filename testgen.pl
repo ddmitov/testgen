af15d5bf520d4729caeff81963e13d6cdaad410c
ddmitov <ddmitov@yahoo.com>
2017-04-25 11:46:03 +0300

    code refactoring

--- testgen.pl
+++ testgen.pl
@@ -14,8 +14,8 @@ use JSON::PP;
 ##################################################
 
 my $number_of_wanted_tests = 10;
-my $number_of_wanted_open_questions_per_test = 6;
-my $number_of_wanted_closed_questions_per_test = 6;
+my $number_of_wanted_open_questions_per_test = 5;
+my $number_of_wanted_closed_questions_per_test = 5;
 
 ##################################################
 
@@ -26,20 +26,23 @@ my $json_data = <$filehandle>;
 close $filehandle;
 
 my $json_object = new JSON::PP;
-my $perl_data = $json_object->decode($json_data);
+my $decoded_json_data = $json_object->decode($json_data);
 
 my $number_of_available_closed_questions;
 my $number_of_available_open_questions;
 my @all_valid_question_numbers;
+
 for (my $number=1; $number <= 99; $number++) {
 	$number = sprintf ("%02d", $number);
-	if (defined ($perl_data->{$number})) {
-		if (length ($perl_data->{$number}->{question}) > 0) {
-			my $question_type = $perl_data->{$number}{question_type};
+	if (defined ($decoded_json_data->{$number})) {
+		if (length ($decoded_json_data->{$number}->{question}) > 0) {
+			my $question_type = $decoded_json_data->{$number}{question_type};
+
 			if ($question_type =~ "closed") {
 				$number_of_available_closed_questions++;
 				push @all_valid_question_numbers, $number;
 			}
+
 			if ($question_type =~ "open") {
 				$number_of_available_open_questions++;
 				push @all_valid_question_numbers, $number;
@@ -61,7 +64,7 @@ if ($number_of_wanted_open_questions_per_test > $number_of_available_open_questi
 print "\nAvailable closed questions: $number_of_available_closed_questions\n";
 print "Available open questions: $number_of_available_open_questions\n";
 
-for (my $test_number=1; $test_number <= $number_of_wanted_tests; $test_number++) {
+for (my $test_number = 1; $test_number <= $number_of_wanted_tests; $test_number++) {
 	print "\nВариант $test_number\n\n";
 
 	my @questions;
@@ -70,15 +73,17 @@ for (my $test_number=1; $test_number <= $number_of_wanted_tests; $test_number++)
 	my $number_of_open_questions = 0;
 
 	until ($number_of_closed_questions == $number_of_wanted_closed_questions_per_test) {
-		my $random_question_number = $all_question_numbers [rand @all_question_numbers];
-		my $random_question;
-		my $question_type;
-		my $date;
+		my $random_question_number = $all_question_numbers[rand @all_question_numbers];
 
-		if (defined ($perl_data->{$random_question_number})) {
-			$random_question = $perl_data->{$random_question_number}{question};
-			$question_type = $perl_data->{$random_question_number}{question_type};
-			$date = $perl_data->{$random_question_number}{date};
+		my $random_question_index = first_index{$_ eq $random_question_number}@all_question_numbers;
+		if (scalar @all_question_numbers > 1) {
+			splice @all_question_numbers, $random_question_index, 1;
+		}
+
+		if (defined ($decoded_json_data->{$random_question_number})) {
+			my $random_question = $decoded_json_data->{$random_question_number}{question};
+			my $question_type = $decoded_json_data->{$random_question_number}{question_type};
+			my $date = $decoded_json_data->{$random_question_number}{date};
 
 			if ($question_type =~ "closed") {
 				$number_of_closed_questions++;
@@ -92,12 +97,13 @@ for (my $test_number=1; $test_number <= $number_of_wanted_tests; $test_number++)
 					for (my $answer_number = 1; $answer_number <= 4; $answer_number++) {
 						my $random_answer_number = $all_variant_numbers [rand @all_variant_numbers];
 
-						my $random_answer_index = first_index {$_ eq $random_answer_number} @all_variant_numbers;
+						my $random_answer_index = first_index{$_ eq $random_answer_number}@all_variant_numbers;
 						if (scalar @all_variant_numbers > 1) {
 							splice @all_variant_numbers, $random_answer_index, 1;
 						}
 
-						my $random_answer = $perl_data->{$random_question_number}{answers}{$random_answer_number};
+						my $random_answer =
+							$decoded_json_data->{$random_question_number}{answers}{$random_answer_number};
 
 						if ($answer_number == 1) {
 							$variant_a = $random_answer;
@@ -113,41 +119,38 @@ for (my $test_number=1; $test_number <= $number_of_wanted_tests; $test_number++)
 						}
 					}
 				} else {
-					$variant_a = $perl_data->{$random_question_number}{answers}{1};
-					$variant_b = $perl_data->{$random_question_number}{answers}{2};
-					$variant_c = $perl_data->{$random_question_number}{answers}{3};
-					$variant_d = $perl_data->{$random_question_number}{answers}{4};
+					$variant_a = $decoded_json_data->{$random_question_number}{answers}{1};
+					$variant_b = $decoded_json_data->{$random_question_number}{answers}{2};
+					$variant_c = $decoded_json_data->{$random_question_number}{answers}{3};
+					$variant_d = $decoded_json_data->{$random_question_number}{answers}{4};
 				}
 
-				push @questions, {question => $random_question, variant_a => $variant_a, variant_b => $variant_b, variant_c => $variant_c, variant_d => $variant_d, date => $date};
-
-				my $random_question_index = first_index {$_ eq $random_question_number} @all_question_numbers;
-				if (scalar @all_question_numbers > 1) {
-					splice @all_question_numbers, $random_question_index, 1;
-				}
+				push @questions, {question => $random_question,
+								variant_a => $variant_a,
+								variant_b => $variant_b,
+								variant_c => $variant_c,
+								variant_d => $variant_d,
+								date => $date};
 			}
 		}
 	}
 
 	until ($number_of_open_questions == $number_of_wanted_open_questions_per_test) {
 		my $random_question_number = $all_question_numbers[rand @all_question_numbers];
-		my $random_question;
-		my $question_type;
-		my $date;
 
-		if (defined ($perl_data->{$random_question_number})) {
-			$random_question = $perl_data->{$random_question_number}{question};
-			$question_type = $perl_data->{$random_question_number}{question_type};
-			$date = $perl_data->{$random_question_number}{date};
+		my $random_question_index = first_index{$_ eq $random_question_number}@all_question_numbers;
+		if (scalar @all_question_numbers > 1) {
+			splice @all_question_numbers, $random_question_index, 1;
+		}
+
+		if (defined ($decoded_json_data->{$random_question_number})) {
+			my $random_question = $decoded_json_data->{$random_question_number}{question};
+			my $question_type = $decoded_json_data->{$random_question_number}{question_type};
+			my $date = $decoded_json_data->{$random_question_number}{date};
 
 			if ($question_type =~ "open") {
 				$number_of_open_questions++;
 				push @questions, {question => $random_question, date => $date};
-
-				my $random_question_index = first_index {$_ eq $random_question_number} @all_question_numbers;
-				if (scalar @all_question_numbers > 1) {
-					splice @all_question_numbers, $random_question_index, 1;
-				}
 			}
 		}
 	}
@@ -167,7 +170,9 @@ for (my $test_number=1; $test_number <= $number_of_wanted_tests; $test_number++)
 			print "в) $sorted_questions[$sorted_question_number]{variant_c}, ";
 		}
 		if (defined ($sorted_questions[$sorted_question_number]{variant_d})) {
-			print "г) $sorted_questions[$sorted_question_number]{variant_d}.\n";
+			my $answer = "г) ".$sorted_questions[$sorted_question_number]{variant_a}.".\n";
+			$answer =~ s/\.\.\n/\.\n/;
+			print $answer;
 		}
 		print "\n";
 	}
